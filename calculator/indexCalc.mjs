f585f861549a797691b91456b9e060b41d98d784
Anastasiia Kholod <121211989+asyaruda@users.noreply.github.com>
2023-08-28 20:12:52 +0300

    Add files via upload

---
+++ calculator/indexCalc.mjs
@@ -0,0 +1,16 @@
+export function addCount(a, b) {
+    return a + b
+  }
+  
+export function subCount(a, b) {
+    return a - b
+  }
+  
+export  function multCount(a, b) {
+    return a * b
+  }
+  
+export  function divCount(a, b) {
+    return a / b
+  }
+  
